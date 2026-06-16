6b190edf0f825c6a63600f5ad9a687b6aefab00f
Marco Cayuela <marco.cayuela@gmail.com>
2026-06-16 16:40:43 +0200

    dataset_manager

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -53,7 +53,6 @@ class Trainer():
         for batch_idx, (inputs, targets) in batch_bar:
 
             x_t, targets = inputs.to(self.device).float(), targets.to(self.device).float()
-            print(targets.shape)
             self.optimizer.zero_grad()
 
             outputs = torch.empty_like(targets)
